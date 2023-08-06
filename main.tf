a1b37c49c0e90ac8a77cdadc7db2a12ce30a4456
melovagabond <dcbranche246@gmail.com>
2023-08-06 12:48:20 -0400

    Modified Jenkinsfile and security routes

--- main.tf
+++ main.tf
@@ -56,9 +56,9 @@ resource "azurerm_network_security_rule" "daevonlab_dev_rule" {
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
-  protocol                    = "Tcp"
+  protocol                    = "*"
   source_port_range           = "*"
-  destination_port_range      = "443"
+  destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.daevonlab_rg.name
